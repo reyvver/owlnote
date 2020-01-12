5ad8b483a94f8fadab0020f2ddcc80090e9e36d2
tatiana dragunova <49490142+reyvver@users.noreply.github.com>
2020-01-12 20:26:06 +0700

    First add

---
+++ owlnote/Assets/My files/Scripts/DatabaseScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Globalization;
+using TMPro;
+
+public class DatabaseScript : MonoBehaviour
+{
+    public TextMeshProUGUI tMonth, tDayOfWeek;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetDates();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetDates()
+    {
+        string day_of_week = CultureInfo.CurrentCulture.DateTimeFormat.GetDayName(DateTime.Today.DayOfWeek);
+        string month  = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(DateTime.Today.Month);
+        
+        tMonth.text = ToTitleCase(month);
+        tDayOfWeek.text = ToTitleCase(day_of_week);
+    }
+
+    /*Процедура, возвращающая слово с заглавной буквы*/
+    public string ToTitleCase(string str)
+    {
+        return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(str.ToLower());
+    }
+}
---
+++ owlnote/Assets/My files/Scripts/RegistrationScript.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class RegistrationScript: MonoBehaviour {
+
+ // protected Firebase.Auth.FirebaseAuth Auth;
+ // protected Dictionary<string, Firebase.Auth.FirebaseUser> UserByAuth = new Dictionary<string, Firebase.Auth.FirebaseUser>();
+
+  private Coroutine _Registration;
+  
+  public TextMeshProUGUI Email;
+  public TextMeshProUGUI Password;
+  public TextMeshProUGUI VerifyPassword;
+
+  public void CreateUser()
+  {
+      if (CheckValue())
+      {
+         // _Registration = StartCoroutine(CreateUserByEmail(Email.text, Password.text));
+         CreateUserByEmail(Email.text, Password.text);
+          Debug.Log("Done");
+      }
+  }
+
+  private void CreateUserByEmail(string email, string password)
+  {
+      try
+      {
+          //var registration = Auth.CreateUserWithEmailAndPasswordAsync(email, password);
+          // yield return new WaitUntil(predicate: () => registration.IsCompleted);
+
+          //  if (registration.Exception != null)
+          //     Debug.Log("Что-то пошло не так");
+
+          //  _Registration = null;
+      }
+      catch (Exception ex)
+      {
+          Debug.Log(ex.Message);
+      }
+  }
+
+  private bool CheckValue()
+  {
+      bool chk;
+      //Чот не работает это условие,не понимаю почему. Из-за того, что в placeholder что то есть?
+      if (string.IsNullOrEmpty(Email.text) || string.IsNullOrEmpty(Password.text) || string.IsNullOrEmpty(VerifyPassword.text)) {
+          Debug.Log("Не все поля заполнены");
+          chk = false;
+      } 
+      
+      if (Password.text != VerifyPassword.text)
+      {
+          Debug.Log("Введеные пароли не совпадают");
+          chk = false;
+      }
+      else chk = true;
+
+      return chk;
+  }
+  
+
+}
