ae057ccd7c2994e49da675ac5b4d79c3141a1639
tatiana dragunova <49490142+reyvver@users.noreply.github.com>
2020-01-24 23:47:16 +0700

    whyyy

--- owlnote/Assets/Scripts/ManagmentCategories.cs
+++ owlnote/Assets/Scripts/ManagmentCategories.cs
@@ -32,8 +32,7 @@ public class ManagmentCategories : MonoBehaviour
     public GameObject myPrefab;
     public Transform panel;
     public TextMeshProUGUI name, color, description;
-
-    private List<int> categoriesId = new List<int>();
+    
     private bool HideOrShow = false;
     private int currentCount;
     private float startPositionX = -17, startPositionY = 700, space = 0; 
@@ -45,7 +44,6 @@ public class ManagmentCategories : MonoBehaviour
        AddNewCategoryPanel.SetActive(HideOrShow);
        HideOtherStaff.SetActive(HideOrShow);
        InitializeDatabase();
-       GetCurrentCount();
     }
     /*Соединяется с БД*/
     private void InitializeDatabase()
@@ -82,135 +80,18 @@ public class ManagmentCategories : MonoBehaviour
         {
             Category newCategory = new Category( name.text, color.text, description.text);
             string json = JsonUtility.ToJson(newCategory);
-            myRef.Child(FirebaseAuth.DefaultInstance.CurrentUser.UserId).Child("categories").Child(currentCount.ToString()).SetRawJsonValueAsync(json);
-            UpdateCurrentCount(currentCount++);
+            myRef.Child(FirebaseAuth.DefaultInstance.CurrentUser.UserId).Child("categories").Push()
+                .SetRawJsonValueAsync(json);
             ShowAddMenu();
             ClearTextBoxes();
         }
     }
-    /*Узнать количество категорий*/
-    private void GetCurrentCount()
-    {
-        myRef.Child(FirebaseAuth.DefaultInstance.CurrentUser.UserId).Child("categories")
-            .Child("countCategories")
-            .GetValueAsync().ContinueWith(task => {
-                if (task.IsFaulted)
-                {
-                    currentCount = -1;
-                }
-                else if (task.IsCompleted) {
-                    DataSnapshot snapshot = task.Result;
-                    currentCount = Convert.ToInt32(snapshot.Value);
-                }
-            });
-    }
-    /*Возвращает все id*/
-    public void GetCategoriesId()
-    {
-        DatabaseReference rf = myRef.Child(FirebaseAuth.DefaultInstance.CurrentUser.UserId).Child("categories");
-        rf.GetValueAsync().ContinueWith(task => {
-                if (task.IsFaulted)
-                {
-                    currentCount = -1;
-                }
-                else if (task.IsCompleted) {
-                    DataSnapshot snapshot = task.Result;
-                    foreach (var childSnapshot in snapshot.Children)
-                    {
-                        var id = childSnapshot.Key;
-                        Regex rg = new Regex(@"\D");
-                        if (!rg.IsMatch(id))
-                        {
-                            categoriesId.Add(Convert.ToInt32(id));
-                        }
-                    }
-                    Debug.Log(categoriesId.Count);
-                }
-            });
-        Debug.Log(categoriesId.Count);
-    }
-    /*Обновить после выполнения операции добавления или удаления*/
-    private void UpdateCurrentCount(int newNumber)
-    {
-        myRef.Child(FirebaseAuth.DefaultInstance.CurrentUser.UserId).Child("categories").Child("countCategories")
-            .SetValueAsync(currentCount).ContinueWith(task => {
-                if (task.IsFaulted)
-                {
-                    Debug.Log("me");
-                }
-                else if (task.IsCompleted) {
-                   Debug.Log("nice");
-                }
-            });
-    }
-    /*Очистить поля после выполнения операции*/
     private void ClearTextBoxes()
     {
         name.text = "";
         description.text = "";
         color.text = "красный";
     }
-    /*Визуализация*/
-    public void ViewCategories()
-    {
-       // categoriesId.Clear();
-        GetCategoriesId();
-        GenerateCategories();
-    }
-
-    private void GenerateCategories()
-    {
-
-        try
-        {
-            for (int i = 0; i < currentCount; i++)
-            {
-                GameObject myobj = Instantiate(myPrefab, new Vector3(startPositionX, (startPositionY-space), 0), Quaternion.identity) as GameObject;
-                myobj.transform.SetParent(panel.transform,false);
-                space += 260;
-            }
-
-          //  GenerateTextCategories();
-        }
-        catch (Exception e)
-        {
-            Debug.Log(e);
-        }
-    }
-    private void GenerateTextCategories()
-    {  
-        GameObject panel = GameObject.Find("ListOfCategories");
-        int i = 0;
-        foreach (Transform obj in  panel.transform)
-        {
-            GameObject name = obj.Find("CategoryName").gameObject;
-            GameObject description = obj.Find("CategoryDescription").gameObject;
-            GameObject color = obj.Find("CategoryColor").gameObject;
-
-            name.GetComponent<TextMeshProUGUI>().text = GetValues(categoriesId[i], "name");
-            description.GetComponent<TextMeshProUGUI>().text = GetValues(categoriesId[i], "description");
-            color.GetComponent<Image>().color = Color.cyan;
-            i++;
-        }
-    }
-    /*Получить значение свойства текущей категории*/
-    private string GetValues(int id,string propety)
-    {
-        string result = "empty";
-        myRef.Child(FirebaseAuth.DefaultInstance.CurrentUser.UserId).Child("categories").Child(id.ToString()).Child(propety)
-            .GetValueAsync().ContinueWith(task => {
-                if (task.IsFaulted)
-                {
-                    Debug.Log("error occured");
-                }
-                else if (task.IsCompleted) {
-                    DataSnapshot snapshot = task.Result;
-                    result = snapshot.Value.ToString();
-                }
-            });
-        return result;
-    }
-    /*Показать/скрыть меню добавления*/
     public void ShowAddMenu()
     {
         HideOrShow = !HideOrShow;
@@ -226,4 +107,27 @@ public class ManagmentCategories : MonoBehaviour
             ButtonAdd.transform.Rotate(0,0,45);
         }
     }
+
+    public void ViewCategories()
+    {
+        GetCount();
+        Debug.Log(currentCount);
+    }
+
+    private void GetCount()
+    {
+        DatabaseReference newRef = myRef.Child(FirebaseAuth.DefaultInstance.CurrentUser.UserId).Child("categories").Reference;
+        newRef.GetValueAsync().ContinueWith(task => {
+            if (task.IsFaulted)
+            {
+                Debug.Log("error occured");
+            }
+            else if (task.IsCompleted) {
+                DataSnapshot snapshot = task.Result;
+                currentCount = Convert.ToInt32(snapshot.ChildrenCount);
+            }
+        });
+    }
+
+
 }
